5391325c49a5a8f7e8d327b888f43e2d5b8a8516
Tqkhan <Tqkhan@users.noreply.github.com>
2018-07-13 15:54:49 +0500

    asd

--- application/config/config.php
+++ application/config/config.php
@@ -23,12 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
-$config['base_url'] = 'http://localhost/itgs_july';
-=======
+
+
 
 $config['base_url'] = 'http://localhost/itgs/';
->>>>>>> 3557c26af5dfc3d530b45d4f906566b480b1503c
 
 /*
 |--------------------------------------------------------------------------
